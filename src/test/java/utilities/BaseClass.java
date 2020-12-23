9125309fae3479937688335efd5452823d3cf949
Waqas Khan <wuikhan@gmail.com>
2020-12-22 21:40:21 -0500

    check if it runs

--- src/test/java/utilities/BaseClass.java
+++ src/test/java/utilities/BaseClass.java
@@ -14,6 +14,7 @@ public class BaseClass {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
+		
 	}
 
 }
